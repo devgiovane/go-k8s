dbcb5447de678d7daad9cdb55644bfe50dc19b0f
Giovane Santos <giovanesantos1999@gmail.com>
2025-10-22 09:02:01 -0300

    fix(config): add secrets and health check

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"os"
+	"time"
 )
 
 func main() {
+	startedAt := time.Now()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		name := os.Getenv("NAME")
-		fmt.Fprintf(w, "Hello, I'm %s.", name)
+		uptime := time.Since(startedAt)
+		if uptime.Seconds() < 10 {
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(200)
+	})
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		// w.Write([]byte("ok"))
 	})
 	http.ListenAndServe(":80", nil)
 }
